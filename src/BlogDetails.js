7b081d8cd4d8daaa849bc481faec672fc456ce41
happyjamy <juwhan1004@naver.com>
2023-01-07 00:25:01 +0900

    dw

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -2,32 +2,36 @@ import { useNavigate, useParams } from "react-router-dom";
 import useFetch from "./useFetch";
 
 const BlogDetails = () => {
-    const { id } = useParams();
-    const { data: blog, isLoading, err } = useFetch("http://localhost:8000/blogs/" + id);
-    const navigate = useNavigate();
+  const { id } = useParams();
+  const {
+    data: blog,
+    isLoading,
+    err,
+  } = useFetch("http://3.39.207.95:8080/post/" + id);
+  const navigate = useNavigate();
 
-    const handleClick = () => {
-        fetch('http://localhost:8000/blogs/' + blog.id, {
-            method: 'DELETE'
-        }).then(() => {
-            navigate('/');
-        })
-    }
+  const handleClick = () => {
+    fetch("http://3.39.207.95:8080/post/" + blog.id, {
+      method: "DELETE",
+    }).then(() => {
+      navigate("/");
+    });
+  };
 
-    return (
-        <div className="blog-details">
-            {isLoading && <div>Loading....</div>}
-            {err && <div>{err}</div>}
-            {blog && (
-                <article>
-                    <h2>{blog.title}</h2>
-                    <p>Written by {blog.author}</p>
-                    <div>{blog.body}</div>
-                    <button onClick={handleClick}>Delete</button>
-                </article>
-            )}
-        </div>
-    );
-}
- 
+  return (
+    <div className="blog-details">
+      {isLoading && <div>Loading....</div>}
+      {err && <div>{err}</div>}
+      {blog && (
+        <article>
+          <h2>{blog.title}</h2>
+          <p>Written by {blog.author}</p>
+          <div>{blog.body}</div>
+          <button onClick={handleClick}>Delete</button>
+        </article>
+      )}
+    </div>
+  );
+};
+
 export default BlogDetails;
--- src/Home.js
+++ src/Home.js
@@ -2,15 +2,19 @@ import BlogList from "./BlogList";
 import useFetch from "./useFetch";
 
 const Home = () => {
-    const { data: blogs, isLoading, err } = useFetch("http://localhost:8000/blogs");
+  const {
+    data: blogs,
+    isLoading,
+    err,
+  } = useFetch("http://3.39.207.95:8080/blogs");
 
-    return (
-        <div className="home">
-            {err && <div>{ err }</div>}
-            {isLoading && <div>Loading....</div>}
-            {blogs && <BlogList blogs={blogs} title="All Blogs !" />}
-        </div>
-    );
-}
- 
+  return (
+    <div className="home">
+      {err && <div>{err}</div>}
+      {isLoading && <div>Loading....</div>}
+      {blogs && <BlogList blogs={blogs} title="All Blogs !" />}
+    </div>
+  );
+};
+
 export default Home;
